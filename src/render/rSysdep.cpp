4cbe223de2f9ab524834bd2d3208bba2999d34de
yarrt <Unknown>
2006-06-07 04:48:34 +0000

    added missing unistd.h

--- src/render/rSysdep.cpp
+++ src/render/rSysdep.cpp
@@ -46,6 +46,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #ifndef DEDICATED
 #include <png.h>
+#include <unistd.h>
 #define SCREENSHOT_PNG_BITDEPTH 8
 #define SCREENSHOT_BYTES_PER_PIXEL 3
 #ifndef SDL_OPENGL
