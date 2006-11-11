d3d2073491c9f780ca5ed3fb15011646093fe92e
nemostultae <Unknown>
2006-11-11 14:20:06 +0000

    rotator.rb now rotates maps every round

--- scripts/rotator.rb
+++ scripts/rotator.rb
@@ -1,27 +1,39 @@
 require "armagetronad"
 
 class Rotator
+  
+  MAPS = %w[Your_mom/clever/repeat-0.3.2.aamap.xml
+            Your_mom/clever/inaktek-0.7.2.aamap.xml
+            Anonymous/polygon/regular/40-gon-0.1.1.aamap.xml
+            Anonymous/polygon/regular/diamond-1.0.2.aamap.xml
+            Z-Man/fortress/zonetest-0.1.0.aamap.xml]
+  
   def initialize
     @rounds_played = 0
     @matches_played = 0
+    @map = 0
   end
   
   def round_event
     @rounds_played += 1
-    puts "rounds_played = #{@rounds_played}"
-    
-    # even rounds are faster
-    if (@rounds_played % 2).zero?
-      ArmagetronAd::Tools::ConfItemBase.load_line("speed_factor 4")
-    else
-      ArmagetronAd::Tools::ConfItemBase.load_line("speed_factor 0")
-    end
+    puts "rounds_played = #{@rounds_played}"    
+    ArmagetronAd::Tools::ConfItemBase.load_line("map_file #{next_map()}")
   end
   
   def match_event
     @matches_played += 1
     puts "matches_played = #{@matches_played}"
   end
+  
+  def next_map
+    @map += 1
+    # ring around
+    if @map > MAPS.length - 1
+      @map = 0
+    end
+    MAPS[@map]      
+  end
+  
 end
 
 rotator = Rotator.new
