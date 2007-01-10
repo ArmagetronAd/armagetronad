a158d94e9721dcf3dc398af220afe8c48da8e49f
z-man <Unknown>
2007-01-10 20:21:14 +0000

    Windows build: Moved config.h to aa_config.h.Why doesn't SVN move work any more? Oh well.

---
+++ src/win32/aa_config.h
@@ -0,0 +1,12 @@
+// Windows client configuration header
+
+#ifndef CONFIG_H_INCLUDED
+#define CONFIG_H_INCLUDED
+
+// include common Windows header
+#include "config_common.h"
+
+// we're using SDL_Mixer
+#define HAVE_LIBSDL_MIXER 1
+
+#endif
