e557e40c3bff821263eb1014bf9419f177db4788
z-man <Unknown>
2007-12-14 17:13:25 +0000

    Calling PumpEvents() while waiting for application to receive focus; only that way, it actually gets noticed.

--- src/render/rScreen.cpp
+++ src/render/rScreen.cpp
@@ -707,7 +707,10 @@ static bool lowlevel_sr_InitDisplay(){
 
     // wait for activation if we were ALT-Tabbed away:
     while ( (SDL_GetAppState() & SDL_APPACTIVE) == 0)
+    {
         SDL_Delay(100);
+        SDL_PumpEvents();
+    }
 
     if (software_renderer && !last_software_renderer)
         sr_LoadDefaultConfig();
@@ -717,7 +720,10 @@ static bool lowlevel_sr_InitDisplay(){
 
     // wait for activation if we were ALT-Tabbed away:
     while ( (SDL_GetAppState() & SDL_APPACTIVE) == 0)
+    {
         SDL_Delay(100);
+        SDL_PumpEvents();
+    }
 
     sr_ResetRenderState(true);
 
