cbc160dd9e1d52762d197353b63bc82c8f4bafa7
z-man <Unknown>
2008-02-09 15:13:12 +0000

    Experimental: activating ZThread usage and Armathentication on Windows dedicated server build.

--- src/win32_ded/config.h
+++ src/win32_ded/config.h
@@ -6,6 +6,10 @@
 // define if you wish to compile a dedicated server
 #define DEDICATED 1
 
+// activate armathentication support
+#define HAVE_LIBZTHREAD 1
+#define KRAWALL_SERVER 1
+
 // include common Windows header
 #include "win32/config_common.h"
 
