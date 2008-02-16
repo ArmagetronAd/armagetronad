cea9f6cf99f08b2326148fccb85fb1dfb0d9ae16
Manuel Moos <z-man@users.sf.net>
2008-02-16 23:52:16 +0100

    Giving glyphs the right orientation so they don't get invisible when nobody else bothers to disable backface culling.

--- src/render/rFont.cpp
+++ src/render/rFont.cpp
@@ -164,17 +164,17 @@ void rFont::Render(unsigned char c,REAL left,REAL top,REAL right,REAL bot){
             select->Select(true);
             sr_lastSelected = select;
         }
-
+        
         BeginQuads();
 
-        glTexCoord2f(tleft,ttop);
-        glVertex2f(   left, top);
+        glTexCoord2f(tright,tbot);
+        glVertex2f(   right, bot);
 
         glTexCoord2f(tright,ttop);
         glVertex2f(   right ,top);
 
-        glTexCoord2f(tright,tbot);
-        glVertex2f(   right, bot);
+        glTexCoord2f(tleft,ttop);
+        glVertex2f(   left, top);
 
         glTexCoord2f(tleft,tbot);
         glVertex2f(   left, bot);
@@ -302,13 +302,14 @@ void rTextField::FlushLine(int len,bool newline){
                 REAL b=t - cheight;
 
                 BeginQuads();
-                glVertex2f(   l, t);
 
-                glVertex2f(   r ,t);
+                glVertex2f(   l, b);
 
                 glVertex2f(   r, b);
 
-                glVertex2f(   l, b);
+                glVertex2f(   r ,t);
+
+                glVertex2f(   l, t);
             }
             else
             {
