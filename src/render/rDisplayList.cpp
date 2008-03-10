873497295551612b38b6266aebf913f89f704b27
Manuel Moos <z-man@users.sf.net>
2008-03-10 21:46:37 +0100

    Disabling silly displaylist stats for the dedicated server.

--- src/render/rDisplayList.cpp
+++ src/render/rDisplayList.cpp
@@ -30,7 +30,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "rScreen.h"
 
 #ifdef DEBUG
+#ifndef DEDICATED
 #define LIST_STATS
+#endif
 #endif
 
 #ifdef LIST_STATS
