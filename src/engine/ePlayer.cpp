4fa495753b818974fc607b248a3f4f8d9448532a
epsy46 <epsy46@free.fr>
2008-04-20 01:39:23 +0200

    /admin KILL could kill dead people sometimes, also added /admin SLAP, to enforce the badmin's authority.

--- src/engine/ePlayer.cpp
+++ src/engine/ePlayer.cpp
@@ -6825,7 +6825,8 @@ static void Kill_conf(std::istream &s)
     }
 
     ePlayerNetID * p = ReadPlayer( s );
-    if ( p && p->Object() )
+
+    if ( p && p->Object() && p->Object()->Alive() )
     {
         p->Object()->Kill();
         sn_ConsoleOut( tOutput( "$player_admin_kill", p->GetColoredName() ) );
@@ -6834,6 +6835,39 @@ static void Kill_conf(std::istream &s)
 
 static tConfItemFunc kill_conf("KILL",&Kill_conf);
 static tAccessLevelSetter se_killConfLevel( kill_conf, tAccessLevel_Moderator );
+
+static void Slap_conf(std::istream &s)
+{
+    if ( se_NeedsServer( "SLAP", s, false ) )
+    {
+        return;
+    }
+
+    ePlayerNetID * victim = ReadPlayer( s );
+
+    if ( victim )
+    {
+	int points = 1;
+
+	s >> points;
+
+	if ( points == 0)
+	{
+	    // oh well :)
+	    sn_ConsoleOut( tOutput("$player_admin_slap_free", victim->GetColoredName() ) );
+	}
+
+	tOutput win;
+	tOutput lose;
+	win << "$player_admin_slap_win";
+	lose << "$player_admin_slap_lose";
+        victim->AddScore( -points, win, lose );
+    }
+}
+
+static tConfItemFunc slap_conf("SLAP", &Slap_conf);
+static tAccessLevelSetter se_slapConfLevel( slap_conf, tAccessLevel_Admin );
+
 
 static void Suspend_conf_base(std::istream &s, int rounds )
 {
