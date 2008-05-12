916abcb2622a192178cfa306d5f4329d314d0a78
epsy46 <epsy46@free.fr>
2008-05-12 13:05:50 +0200

    include nNetwork.h in ePlayer.cpp (hoax try this)

--- src/engine/ePlayer.cpp
+++ src/engine/ePlayer.cpp
@@ -54,6 +54,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "nServerInfo.h"
 #include "tRecorder.h"
 #include "nConfig.h"
+#include "nNetwork.h"
 #include <time.h>
 
 
