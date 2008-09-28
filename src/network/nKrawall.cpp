69355a90f23ddefcf04a34bcc1bd08a149091130
epsy <epsy46@free.fr>
2008-09-28 23:11:38 +0200

    allow an authority to claim one to be from another authority, as long as the two authorities share the same base domain

--- src/network/nKrawall.cpp
+++ src/network/nKrawall.cpp
@@ -474,6 +474,37 @@ void nKrawall::SplitUserName( tString const & original, tString & username, tStr
     authority = "";
 }
 
+void nKrawall::SplitBaseAuthorityName( tString const & authority, tString & base )
+{
+    for( int i = 1; i < authority.Len(); ++i )
+    {
+        if ( authority[i] == '/' )
+        {
+                base = authority.SubStr( 0, i );
+                return;
+        }
+    }
+    base = authority;
+    return;
+}
+
+// Tell if an authority has the right to claim that the user uses another authority
+bool nKrawall::CanClaim ( tString contacted, tString claimed )
+{
+    // Check if the claimed authority is just the one we contacted
+    if ( contacted == claimed )
+        return true;
+
+    // It could be claiming the user is from a subdirectory:
+    tString contactedBase, claimedBase;
+    SplitBaseAuthorityName( contacted, contactedBase );
+    SplitBaseAuthorityName( claimed, claimedBase );
+    if ( claimedBase == contactedBase )
+        return true;
+
+    return false;
+}
+
 #ifdef KRAWALL_SERVER_LEAGUE
 
 // called on the master server when the league message is received
