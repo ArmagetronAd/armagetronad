01b57ecdfc4cd0095185c3fcae4dcac4302f9233
Manuel Moos <z-man@users.sf.net>
2009-02-16 15:33:41 +0100

    Implemented a variant of RLBOT simulating tripple binds. Useful for stress tests.

--- src/tron/gCycle.cpp
+++ src/tron/gCycle.cpp
@@ -474,6 +474,10 @@ class Sensor: public gSensor
             , turnedRecently_ ( 0 )
             , owner_ ( owner )
     {
+#ifdef RLBOT
+        rlDir = 1;
+        rlLastTime = -100;
+#endif
     }
 
     // describes walls we like. We like enemy walls. We like to go between them.
@@ -625,9 +629,51 @@ class Sensor: public gSensor
         return owner_->CanMakeTurn( ( action == &gCycle::se_turnRight ) ? 1 : -1 );
     }
 
+#ifdef RLBOT
+    int rlDir;
+    REAL rlLastTime;
+#endif
+
     // does the main thinking
     void Activate( REAL currentTime )
     {
+#ifdef RLBOT
+        // hack chatbot for crazy turning
+        {
+            if (!owner_->Alive() || !owner_->Vulnerable() )
+            {
+                return;
+            }
+            if( fabs( rlLastTime - currentTime) > 1 )
+            {
+                owner_->Act( &gCycle::se_turnRight, 1 );
+                rlDir = -1;
+            }
+            else  if ( rlDir > 0 )
+            {
+                if( CanMakeTurn( &gCycle::se_turnRight ) )
+                {
+                    owner_->Act( &gCycle::se_turnRight, 1 );
+                    owner_->Act( &gCycle::se_turnRight, 1 );
+                    owner_->Act( &gCycle::se_turnRight, 1 );
+                    rlDir = -1;
+                }
+            }
+            else
+            {
+                if( CanMakeTurn( &gCycle::se_turnLeft ) )
+                {
+                    owner_->Act( &gCycle::se_turnLeft, 1 );
+                    owner_->Act( &gCycle::se_turnLeft, 1 );
+                    owner_->Act( &gCycle::se_turnLeft, 1 );
+                    rlDir = 1;
+                }
+            }
+            rlLastTime = currentTime;
+            return;
+        }
+#endif
+
         // is it already time for activation?
         if ( currentTime < nextChatAI_ )
             return;
