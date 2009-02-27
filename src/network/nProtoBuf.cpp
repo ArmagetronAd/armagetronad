a3cd20b5edd2b66e05cb6ff12e6607e82bd5c2a2
Manuel Moos <z-man@users.sf.net>
2009-02-27 22:01:45 +0100

    Removed zlib header for now, we're not actually using it.

--- src/network/nProtoBuf.cpp
+++ src/network/nProtoBuf.cpp
@@ -42,7 +42,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "tLocale.h"
 #include "tConfiguration.h"
 
-#include <zlib.h>
+// #include <zlib.h>
 
 using namespace google::protobuf;
 
