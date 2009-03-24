51c8bfe3d38807122ba5e4589e14bc4517c101ef
Daniel Lee Harple <leeharple@gmail.com>
2009-03-24 12:52:54 -0400

    cycleprograminited didn't respect the project dependency structure.

--- src/render/rScreen.cpp
+++ src/render/rScreen.cpp
@@ -751,7 +751,7 @@ static bool lowlevel_sr_InitDisplay(){
     return true;
 }
 
-extern bool cycleprograminited;
+bool cycleprograminited = false;
 
 bool sr_InitDisplay(){
     cycleprograminited = false;
