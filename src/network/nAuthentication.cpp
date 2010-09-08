daf6bd7f0d269a7953a8d8011e157a60d1691b95
Manuel Moos <z-man@users.sf.net>
2010-09-08 17:36:53 +0200

    Avoiding costy work on login processes where the user in question is long gone.

--- src/network/nAuthentication.cpp
+++ src/network/nAuthentication.cpp
@@ -432,6 +432,12 @@ public:
     {
     }
 
+    // returns true if this process is still to be considered active
+    bool IsActive() const
+    {
+        return IsInList() && sn_UserID( user ) > 0;
+    }
+
     static nLoginProcess * Find( int userID )
     {
         nMachine & machine = nMachine::GetMachine( userID );
@@ -535,6 +541,12 @@ void nLoginProcess::FetchInfoFromAuthority()
     method.prefix = "";
     method.suffix = "";
     
+    if( !IsActive() )
+    {
+        Abort();
+        return;
+    }
+
     bool ret = false;
     if ( !tRecorder::IsPlayingBack() )
     {
@@ -868,7 +880,7 @@ void nLoginProcess::QueryFromClient()
     // check whether the user disappeared by now (this is run in the main thread,
     // so no risk of the user disconnecting while the function runs)
     int userID = sn_UserID( user );
-    if ( userID <= 0 )
+    if ( !IsActive() )
         return;
 
     // create a random salt value
@@ -1031,6 +1043,11 @@ bool nLoginProcess::CheckServerAddress()
 // and determines whether the client is authorized or not.
 void nLoginProcess::Authorize()
 {
+    if( !IsActive() )
+    {
+        Abort();
+    }
+
     if ( aborted )
     {
         success = false;
@@ -1081,7 +1098,7 @@ void nLoginProcess::Finish()
 {
     // again, userID is safe in this function
     int userID = sn_UserID( user );
-    if ( userID <= 0 )
+    if ( !IsActive() )
         return;
 
     // decorate console with correct sender ID
