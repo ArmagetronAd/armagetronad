cd5570ba1a46952cdab4edfedb99e0dc44e99ea5
z-man <Unknown>
2011-02-01 20:51:23 +0000

    Merging branch 0.2.8 from revision 9508 to 9514:  ------------------------------------------------------------------------  r9514 | bazaaarmagetron | 2011-02-01 21:33:07 +0100 (Tue, 01 Feb 2011) | 2 lines
     Manuel Moos: Removing timebot detecion code stats for the client even in debug mode.
     
     ------------------------------------------------------------------------
     r9513 | bazaaarmagetron | 2011-02-01 21:32:21 +0100 (Tue, 01 Feb 2011) | 2 lines
     
     Manuel Moos: Fixed unsigned score calculation bug in fortress conquest. Titanoboa discovered negative FORTRESS_CONQUERED_SCORE has a comical effect.
     
     ------------------------------------------------------------------------

--- src/engine/ePlayer.cpp
+++ src/engine/ePlayer.cpp
@@ -9363,7 +9363,9 @@ void ePlayerNetID::AnalyzeTiming( REAL timing )
 eUncannyTimingDetector::eUncannyTimingSettings::~eUncannyTimingSettings()
 {
 #ifdef DEBUG
+#ifdef DEDICATED
     con << "Best ratio achieved for " << timescale*1000 << "ms stat: " << bestRatio << "\n";
+#endif
 #endif
 }
 
