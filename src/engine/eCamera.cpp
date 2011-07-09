1ac6c7a62c43ce30512e32e59d785b450435b8b7
Manuel Moos <z-man@users.sf.net>
2011-07-09 21:02:32 +0200

    Fixing camera frantically zooming away if you die in incam mode and nobody else is around.

--- src/engine/eCamera.cpp
+++ src/engine/eCamera.cpp
@@ -1918,7 +1918,7 @@ void eCamera::Timestep(REAL ts){
     if (!CenterAlive() && (newmode==CAMERA_IN || newmode==CAMERA_SMART_IN)){// || newmode==CAMERA_CUSTOM || newmode==CAMERA_SERVER_CUSTOM)){
         pos=pos-dir.Turn(eCoord(5,1));
         z+=2;
-        mode = localPlayer ? localPlayer->startCamera : CAMERA_SMART;
+        mode = ( localPlayer && localPlayer->startCamera != CAMERA_IN ) ? localPlayer->startCamera : CAMERA_SMART;
     }
 
     const REAL dirSmooth = se_cameraSmartCenterDirSmooth;
