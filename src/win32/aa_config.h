71239e0fa4b62b7c137915e5abff6f75222d9ae2
Manuel Moos <manuel@moosnet.de>
2011-07-29 14:55:18 +0200

    isblank() appeared, so adding HAVE_ISBLANK.

--- src/win32/aa_config.h
+++ src/win32/aa_config.h
@@ -6,6 +6,9 @@
 // include common Windows header
 #include "config_common.h"
 
+// is now defined for code::blocks > 8 or so
+#define HAVE_ISBLANK
+
 // we're using SDL_Mixer
 #define HAVE_LIBSDL_MIXER 1
 
