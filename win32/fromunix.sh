92e171d6d974dfb411d40d66c0aef9a3dda0eb65
Manuel Moos <z-man@users.sf.net>
2011-09-01 11:46:02 +0200

    Added wine compilation script.

---
+++ win32/fromunix.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Complies Windows client and server from Linux with Wine set up and
+# code::blocks installed therein in the default directory.
+# Only works from within unpacked source zip since it skips the
+# version generation step.
+
+# compile protobuf
+pushd ../src/protobuf
+wine ..\..\..\winlibs\protobuf\bin\protoc *.proto --cpp_out=.
+popd
+
+pushd code_blocks
+# batch build (assume winlibs is completely built)
+#wine "C:\Program Files\CodeBlocks\codeblocks.exe" /ns /nd ArmagetronAd.cbp --build --target=release
+#wine "C:\Program Files\CodeBlocks\codeblocks.exe" /ns /nd Dedicated.cbp --build --target=release
+wine "C:\Program Files\CodeBlocks\codeblocks.exe" /ns /nd ArmagetronAd.workspace --build --target="Win32 Release"
+popd
+wait
+
+# copy files
+echo -e "makedist.bat\n exit" | wine cmd
+
