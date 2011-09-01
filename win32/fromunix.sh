f598b04083b83b74b7d2442106fa4ffcb759259a
Manuel Moos <z-man@users.sf.net>
2011-09-01 12:12:27 +0200

    Error detection and ingoring.

--- win32/fromunix.sh
+++ win32/fromunix.sh
@@ -7,17 +7,20 @@
 
 # compile protobuf
 pushd ../src/protobuf
-wine ..\..\..\winlibs\protobuf\bin\protoc *.proto --cpp_out=.
+wine ../../../winlibs/protobuf/bin/protoc *.proto --cpp_out=. || exit 1
 popd
 
 pushd code_blocks
 # batch build (assume winlibs is completely built)
 #wine "C:\Program Files\CodeBlocks\codeblocks.exe" /ns /nd ArmagetronAd.cbp --build --target=release
 #wine "C:\Program Files\CodeBlocks\codeblocks.exe" /ns /nd Dedicated.cbp --build --target=release
-wine "C:\Program Files\CodeBlocks\codeblocks.exe" /ns /nd ArmagetronAd.workspace --build --target="Win32 Release"
+wine "C:\Program Files\CodeBlocks\codeblocks.exe" /ns /nd ArmagetronAd.workspace --build --target="Win32 Release" || exit 1
 popd
 wait
 
 # copy files
 echo -e "makedist.bat\n exit" | wine cmd
+
+true
+
 
