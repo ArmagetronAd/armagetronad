b2406a14d7b4d37dab1e9414e630afcf845d401d
z-man <Unknown>
2011-10-21 22:00:44 +0000

    Merging branch 0.2.8 from revision 10063 to 10067:  ------------------------------------------------------------------------  r10066 | bazaaarmagetron | 2011-10-21 23:45:07 +0200 (Fri, 21 Oct 2011) | 2 lines
     Manuel Moos: Fixing assertion failure on clients compiled with --enable-armathentication when they join a server.
     
     ------------------------------------------------------------------------
     r10065 | bazaaarmagetron | 2011-10-21 23:44:18 +0200 (Fri, 21 Oct 2011) | 2 lines
     
     Manuel Moos: Corrected broken SNAFU link.
     
     ------------------------------------------------------------------------

--- src/network/nAuthentication.cpp
+++ src/network/nAuthentication.cpp
@@ -1067,11 +1067,14 @@ static void sn_Reset(){
     int userID = nCallbackLoginLogout::User();
 
     // kill/detach pending login process
-    nLoginProcess * process = nLoginProcess::Find( userID );
-    if ( process )
+    if( sn_GetNetState() == nSERVER )
     {
-        process->Remove();
-        process->Destroy();
+        nLoginProcess * process = nLoginProcess::Find( userID );
+        if ( process )
+        {
+            process->Remove();
+            process->Destroy();
+        }
     }
 }
 
