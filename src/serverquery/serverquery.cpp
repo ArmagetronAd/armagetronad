d4a0ebcf46ce31b9ac8fa0ceb38dd0be8bc2617b
Daniel Lee Harple <leeharple@gmail.com>
2011-11-05 13:20:35 -0400

    Fix case error typo.

--- src/serverquery/serverquery.cpp
+++ src/serverquery/serverquery.cpp
@@ -39,7 +39,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "tConsole.h"
 #include "tDirectories.h"
 #include "tLocale.h"
-#include "tSystime.h"
+#include "tSysTime.h"
 #include "tToDo.h"
 #include "tVersion.h"
 
