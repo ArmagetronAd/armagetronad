14950e3674dfd66db6745dccd65ceb39e15e8456
Manuel Moos <z-man@users.sf.net>
2012-02-20 13:14:18 +0100

    Server browser tooltips are now displayed in small font using the same text output area as the server details, avoiding overlaps; the server lists now shrinks to make room for extra long server details.

--- src/tron/gServerBrowser.cpp
+++ src/tron/gServerBrowser.cpp
@@ -129,6 +129,9 @@ public:
 class gBrowserMenuItem: public uMenuItem
 {
 protected:
+    bool displayHelp_;
+    REAL helpAlpha_;
+    
     gBrowserMenuItem(uMenu *M,const tOutput &help): uMenuItem( M, help )
     {
     }
@@ -137,6 +140,13 @@ protected:
     virtual bool Event( SDL_Event& event );
 
     virtual void RenderBackground();
+
+    virtual bool DisplayHelp( bool display, REAL y, REAL alpha )
+    {
+        helpAlpha_ = alpha;
+        displayHelp_ = display;
+        return false;
+    }
 };
 
 class gServerMenuItem: public gBrowserMenuItem
@@ -683,17 +693,21 @@ void gServerMenuItem::Render(REAL x,REAL y,REAL alpha, bool selected)
 #endif
 }
 
+static REAL sg_menuBottom    = -.9;
+static REAL sg_requestBottom = -.9;
 
 void gServerMenuItem::RenderBackground()
 {
 #ifndef DEDICATED
+    REAL helpTopReal = sg_requestBottom*shrink + displace - .05;;
+
     gBrowserMenuItem::RenderBackground();
 
+    rTextField::SetDefaultColor( tColor(1,1,1) );
+
+    rTextField players( -.9, helpTopReal, text_width, text_height );
     if ( server )
     {
-        rTextField::SetDefaultColor( tColor(1,1,1) );
-
-        rTextField players( -.9, -.3, text_width, text_height );
         players << tOutput( "$network_master_players" );
         if ( server->UserNamesOneLine().Len() > 2 )
             players << server->UserNamesOneLine();
@@ -704,6 +718,28 @@ void gServerMenuItem::RenderBackground()
         uri << server->Url() << tColoredString::ColorString(1,1,1);
         players << tOutput( "$network_master_serverinfo", server->Release(), uri, server->Options() );
     }
+
+    if( displayHelp_ )
+    {
+        players << "\n";
+        players.SetColor(tColor(1,1,1,helpAlpha_));
+        players << Help();
+    }
+
+    REAL helpSpace = players.GetTop() - players.GetBottom();
+    REAL helpTop = -.85 + helpSpace;
+    REAL helpTopScaled = ( helpTop - displace )/shrink;
+    REAL helpTopMax = .25;
+    REAL helpTopMin = -.9;
+    if( helpTopScaled > helpTopMax )
+    {
+        helpTopScaled = helpTopMax;
+    }
+    if( helpTopScaled < helpTopMin )
+    {
+        helpTopScaled = helpTopMin;
+    }
+    sg_requestBottom = helpTopScaled;
 #endif
 }
 
@@ -808,6 +844,24 @@ bool gServerMenuItem::Event( SDL_Event& event )
 
 void gBrowserMenuItem::RenderBackground()
 {
+    if( menu )
+    {
+        double now = tSysTimeFloat();
+        static double lastTime = now;
+        if( sg_menuBottom > sg_requestBottom )
+        {
+            sg_menuBottom -= now - lastTime;
+        }
+        lastTime = now;
+        if( sg_menuBottom < sg_requestBottom )
+        {
+            sg_menuBottom = sg_requestBottom;
+        }
+
+        menu->SetBot( sg_menuBottom );
+        sg_requestBottom = -.9;
+    }
+
     sn_Receive();
     sn_SendPlanned();
 
--- src/ui/uMenu.cpp
+++ src/ui/uMenu.cpp
@@ -325,20 +325,26 @@ void uMenu::OnEnter(){
             if (YPos(menuentries-1)>menuTop && (int(tSysTimeFloat())+1)%2)
                 arrow(.9,menuTop,1,.05);
 
-            if (tSysTimeFloat()-lastkey>timeout){
-                disphelp=true;
+            REAL helpAlpha = tSysTimeFloat()-lastkey-timeout;
+            if( helpAlpha > 1 )
+            {
+                helpAlpha = 1;
+            }
+            
+            disphelp = helpAlpha > 0;
+            if ( items[selected]->DisplayHelp( disphelp, menuBot, helpAlpha ) )
+            {
                 if (sr_alphaBlend)
-                    glColor4f(1,.8,.8,tSysTimeFloat()-lastkey-timeout);
+                    glColor4f(1,.8,.8, helpAlpha );
                 else
-                    Color(tSysTimeFloat()-lastkey-timeout,
-                          .8*(tSysTimeFloat()-lastkey-timeout),
-                          .8*(tSysTimeFloat()-lastkey-timeout));
+                    Color(helpAlpha,
+                          .8*helpAlpha,
+                          .8*helpAlpha);
 
                 rTextField c(-.95f,menuBot-.04f);
                 c.SetWidth(static_cast<int>((1.9f-items[selected]->SpaceRight())/c.GetCWidth()));
                 c << items[selected]->Help();
             }
-            else disphelp=false;
         }
         else
 #endif
