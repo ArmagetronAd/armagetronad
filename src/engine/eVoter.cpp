4e9493cdf6a985d1640f5ad4eef01b4556947554
dlh3 <Unknown>
2013-08-03 05:16:47 +0000

    Merging branch 0.2.8 from revision 10534 to 10541:  ------------------------------------------------------------------------  r10539 | bazaaarmagetron | 2013-08-02 23:13:27 -0400 (Fri, 02 Aug 2013) | 1 line
     Daniel Lee Harple: Don't crash due to assertions in nMachine::GetMachine() when running a client in debug mode.
     ------------------------------------------------------------------------

--- src/engine/eVoter.cpp
+++ src/engine/eVoter.cpp
@@ -2172,6 +2172,9 @@ eVoter* eVoter::GetVoter( int ID, bool complain )
 
 eVoter* eVoter::GetPersistentVoter( int ID )
 {
+    if ( sn_GetNetState() == nCLIENT )
+        return NULL;
+
     // get machine from network subsystem
     nMachine & machine = nMachine::GetMachine( ID );
     return GetPersistentVoter( machine );
@@ -2179,6 +2182,9 @@ eVoter* eVoter::GetPersistentVoter( int ID )
 
 eVoter* eVoter::GetPersistentVoter( nMachine & machine )
 {
+    if ( sn_GetNetState() == nCLIENT )
+        return NULL;
+
     // iterate through the machine's decorators, find a voter
     nMachineDecorator * run = machine.GetDecorators();
     while ( run )
