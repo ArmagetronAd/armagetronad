d0aeb0098605df183375e49e0cb90f2c7ddda028
Daniel Lee Harple <leeharple@gmail.com>
2014-07-18 13:29:38 -0400

    Don't throw errors on the server when it's in the nCLIENT state.

--- src/network/nNetwork.cpp
+++ src/network/nNetwork.cpp
@@ -3637,16 +3637,14 @@ void sn_DisconnectUser(int i, const tOutput& reason, nServerInfoBase * redirectT
         return;
     }
 
-    // clients can only disconnect from the server
-    if ( i != 0 && sn_GetNetState() == nCLIENT )
-    {
-        tERR_ERROR( "Client tried to disconnect from another client: impossible and a bad idea." );
-        return;
-    }
-
     // anything to do at all?
     if (!sn_Connections[i].socket)
     {
+        // clients can only disconnect from the server
+        if ( i != 0 && sn_GetNetState() == nCLIENT )
+        {
+            tERR_ERROR( "Client tried to disconnect from another client: impossible and a bad idea." );
+        }
         return;
     }
 
