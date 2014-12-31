09f9bbf9df1dadf912dcbe23eb021a4c6d75db9b
Dave Fancella <davefancella@gmail.com>
2014-12-30 18:26:00 -0600

    Spurious edit

--- src/tron/gGame.h
+++ src/tron/gGame.h
@@ -24,7 +24,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 ***************************************************************************
 
 */
-
+// spurious edit
 #ifndef ArmageTron_GAME_H
 #define ArmageTron_GAME_H
 
