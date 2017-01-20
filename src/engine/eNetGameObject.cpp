adc976ddbf26b6fd7cec2bbb8950b6a4dc9c51c0
Manuel Moos <z-man@users.sf.net>
2017-01-21 00:03:39 +0100

    Removing any regard for the client transmitted time in obsolete function eNetGameObject::ReceiveControlNet. Leaving the control mechanism around for very old regular clients, even though chances are not good it still works; the IDs used to identify actions are assigned in a broken way and have drifted off.

--- src/engine/eNetGameObject.cpp
+++ src/engine/eNetGameObject.cpp
@@ -73,8 +73,6 @@ void eNetGameObject::MyInitAfterCreation(){
         player->ControlObject(this);
     }
     clientside_action();
-
-    pingOverflow=0;
 }
 
 void eNetGameObject::InitAfterCreation(){
@@ -107,8 +105,6 @@ nNetObject(m){
     autodelete=pid;
 
     laggometerSmooth=laggometer=0;
-
-    pingOverflow=0;
 }
 
 void eNetGameObject::DoRemoveFromGame(){
@@ -165,7 +161,7 @@ void eNetGameObject::Release()
     nNetObject::Release();
 }
 
-// control functions:
+// control functions. Obsolete, regular clients haven't been using this for a long time.
 void eNetGameObject::ReceiveControlNet(nMessage &m){
     REAL time;
     unsigned short act_id;
@@ -175,27 +171,8 @@ void eNetGameObject::ReceiveControlNet(nMessage &m){
     m.Read(act_id);
     m >> x;
 
-    REAL backdate=Lag();//sn_ping[m.SenderID()]*.5;
-    if (backdate>sn_pingCharityServer*.001)
-        backdate=sn_pingCharityServer*.001;
-
-    REAL mintime=se_GameTime()-backdate*1.5-.1;
-    if (time<mintime){
-        con << "mintime\n";
-        REAL pov_needed=mintime-time;
-        if (pov_needed+pingOverflow > MAX_PING_OVERFLOW*backdate){
-            pov_needed = MAX_PING_OVERFLOW*backdate-pingOverflow;
-            con << "Mintime\n";
-        }
-        if (pov_needed<0)
-            pov_needed=0;
-
-        time=mintime-pov_needed;
-        pingOverflow+=pov_needed;
-    }
-
-    if (time>se_GameTime()+1)
-        time=se_GameTime()+1;
+    // don't bother trusting the client sent time in any way
+    time = se_GameTime();
 
     uActionPlayer *Act=uActionPlayer::Find(act_id);
     if ( Act )
@@ -320,9 +297,6 @@ bool eNetGameObject::Timestep(REAL currentTime){
 
     laggometerSmooth=(laggometerSmooth+laggometer*animts)/(1 + animts);
     lastTime=currentTime;
-
-    // Update ping overflow
-    pingOverflow/=(1+animts*PING_OVERFLOW_TS);
 
     return false;
 }
