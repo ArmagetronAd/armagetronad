87ca9465ef74175cc42a3a02962da3eb13deb81f
Manuel Moos <z-man@users.sf.net>
2020-06-04 22:02:09 +0200

    More robust icon and desktop file processing

--- docker/build/context/appimage.sh
+++ docker/build/context/appimage.sh
@@ -11,9 +11,6 @@ if test `getconf LONG_BIT` -eq 32; then
   ARCH=i686
 fi
 
-# copy icon
-cp -l appdir/usr/local/share/games/${PACKAGE_NAME}*/desktop/icons/large/${PACKAGE_NAME}*.png appdir/
-
 #   pack it up. The appimagetool package is available here:
 #   https://github.com/AppImage/AppImageKit/releases
 
@@ -21,4 +18,4 @@ rm -f $1
 ARCH=${ARCH} appimagetool-${ARCH}.AppImage --appimage-extract-and-run appdir $1
 
 # comment out to inspect result for debug purposes
-rm -rf appdir
+# rm -rf appdir
