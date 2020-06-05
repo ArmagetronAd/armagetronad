b1bfea30f48b6f9b90c5861636cbc9e5e58c0942
Manuel Moos <z-man@users.sf.net>
2020-06-05 11:58:30 +0200

    New steam build dockerfile
    Make note of contained libraries in base SDK
    install from base SDK what we can
    take the rest (boost::thread currently) from a more recent Ubuntu

--- docker/images/build_armasteam_64.sh
+++ docker/images/build_armasteam_64.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# builds a basic arma build environment based on alpine, 64 bit
+# builds the steam build system
 
 wd="`dirname $0`"
+sd="${wd}/../scripts"
 . ${wd}/epoch.sh
+bd="${wd}/context/armasteam_64"
+rm -rf ${bd}
+cp -ax ${wd}/armasteam ${bd}
 
 ${wd}/../scripts/ensure_image.sh steamrt_scout_amd64
-${wd}/build_armabuild.sh steamrt_scout_amd64 armasteam_64 --target armabuild_zthreadconfig
+#${wd}/../scripts/ensure_image.sh armabuild_64
+${sd}/build_image.sh steamrt_scout_amd64 armasteam_64 "${bd}" $*
 
