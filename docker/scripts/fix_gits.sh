af212282a0f4ce6eca05a33fb3dedcfedadcac43
Manuel Moos <z-man@users.sf.net>
2020-07-18 18:14:06 +0200

    Set relevant branch to fetch fake submodules
    This switches over to a version of the codeblocks build files that
    has been adapted to work with ap.

--- docker/scripts/fix_gits.sh
+++ docker/scripts/fix_gits.sh
@@ -14,12 +14,12 @@ function fix_git(){
 }
 
 function fix_gits(){
-    ${wd}/ensure_gits.sh legacy_0.2.8 || return $?
+    ${wd}/ensure_gits.sh hack-0.2.8-sty+ct+ap || return $?
 
     set -x
 
     fix_git winlibs d03e20bf8973a6cbd1e0ecb47dd8333c5614b139 || return $?
-    fix_git codeblocks df79e615ab1321415a5d206c9da4a61e1b05d6b4 || return $?
+    fix_git codeblocks 2f363cec627bc40bdc358c2cc467d3384910691c || return $?
     fix_git steam-art 7b3930ab85a3dfbb1caffaa17442b670c823f314 || return $?
     fix_git pkg2appimage 678e5e14122f14a12c54847213585ea803e1f0e1 || return $?
 }
