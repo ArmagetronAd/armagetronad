7c8b502127b69458824badecb97571324690aafc
Manuel Moos <z-man@users.sf.net>
2020-07-29 00:53:24 +0200

    Use proper debian version for release candidates
    One that is considered lower than the final release version.

--- docker/deploy/deploy_ppa.sh
+++ docker/deploy/deploy_ppa.sh
@@ -22,8 +22,8 @@ fi
 SERIES=$1
 test -z ${SERIES} && SERIES=unstable
 
-#ls || exit $?
-DEBIAN_VERSION=`echo ${PACKAGE_VERSION} | sed -e s,_,~,g -e s,-,+,g`~ppa1~${SERIES} || exit $?
+DEBIAN_VERSION=${DEBIAN_VERSION_BASE}~ppa1~${SERIES} || exit $?
+
 CHANGES=${PACKAGE_NAME}_${DEBIAN_VERSION}_source.changes || exit $?
 #ls ${CHANGES} || exit $?
 
