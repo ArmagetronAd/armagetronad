8c3e8aa38bbfc9482f3725704c4e005c06efa347
Manuel Moos <z-man@users.sf.net>
2020-08-30 17:19:17 +0200

    Only rebuild here, do not upload
    Otherwise, the name of the script is confusing.

--- docker/images/rebuild_all_noninteractive.sh
+++ docker/images/rebuild_all_noninteractive.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# rebuilds and uploads those images that don't require interaction
+# rebuilds those images that don't require interaction
 
 wd=$(dirname $0)
 for f in ${wd}/build_*_32.sh ${wd}/build_*_64.sh; do
     echo $f
     ./$f || exit $?
 done
-
-${wd}/upload_all.sh
