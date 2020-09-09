84781a403cb920c26d0799113cc949937cd95285
Manuel Moos <z-man@users.sf.net>
2020-09-09 21:37:07 +0200

    Adapt SDL build
    Take over some debian build settings for SDL
    the usual goals: more compatibility while keeping file size small
    
    Add libraries to armabuild for sound compatibility
    
    Enable shared (dynamic runtime) linkage in SDL
    with X11, alsa and pulseaudio
    
    pulseaudio in particular pulls in FLAC and ogg as dependencies, blowing
    up our appimages. By having SDL load them on demand at runtime, we no
    longer have them as hard dependencies.
    If none of SDL, alsa or pulse are present on the user's system, they don't
    get sound, but they still can play.

--- docker/images/armabuild/build_libsdl.sh
+++ docker/images/armabuild/build_libsdl.sh
@@ -8,8 +8,26 @@ archive=$1
 # if it already exists (Steam SDK), don't bother
 test -d /usr/include/SDL && exit 0
 
-# remove system provided libxml2
-# rm -f /usr/lib/libxml2.so.2 /usr/lib/x86_64-linux-gnu/libxml2.so.2 /usr/lib/i386-linux-gnu/libxml2.so.2
+# from debian
+confflags="--disable-rpath --enable-sdl-dlopen \
+           --disable-video-ggi --disable-video-svga --disable-video-aalib \
+           --disable-nas --disable-esd --disable-arts \
+           "
+
+# not taken from debian
+#           --enable-video-caca \
+# --disable-pulseaudio-shared \
+#           --disable-alsa-shared \
+#           --disable-x11-shared \
+
+# ours
+confflags_ours="--disable-assembly \
+--disable-video-fbcon --disable-video-directfb \
+--disable-x11-dgamouse --disable-video-dga --disable-dga \
+ --disable-alsa-test --enable-alsa --disable-arts --disable-esd --enable-audio \
+ --enable-pulseaudio-shared --enable-alsa-shared --enable-x11-shared"
+
+#--disable-alsa --enable-pulseaudio
 
 # fetch
 curl https://www.libsdl.org/release/${archive}.tar.gz -o ${archive}.tar.gz || exit $?
@@ -20,7 +38,7 @@ rm ${archive}.tar.gz
 cd ${archive}
 patch -Np1 -i ../libsdl-1.2.15-const-xdata32.patch || exit $?
 rm -f ../libsdl-1.2.15-const-xdata32.patch
-CFLAGS=-Os ./configure --prefix=/usr/ || exit $?
+CFLAGS=-Os ./configure --prefix=/usr/ ${confflags_ours} ${confflags} || exit $?
 make -j 5 || exit $?
 make install || exit $?
 cd .. && rm -rf ${archive}
