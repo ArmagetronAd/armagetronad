057f9401d7a819fdaf741ffb2774cf9135ab00b4
Manuel Moos <z-man@users.sf.net>
2020-12-19 18:47:06 +0100

    Work around old tools
    Just for validation, output unchanged: The version of appstreamcli
    does not yet know about OARS (https://hughsie.github.io/oars/index.html),
    so we need to make our appstream data compliant with the old standards.

--- docker/build/context/appimage.sh
+++ docker/build/context/appimage.sh
@@ -8,7 +8,7 @@ set -x
 # determine arch suffix
 ARCH=x86_64
 if test `getconf LONG_BIT` -eq 32; then
-  ARCH=i686
+    ARCH=i686
 fi
 
 # link icon
@@ -17,14 +17,28 @@ cp appdir/usr/share/games/${PACKAGE_NAME}*/desktop/icons/48x48/*.png appdir/.Dir
 # lint according to https://github.com/AppImage/appimage.github.io#checklist-for-submitting-your-own-appimage
 chmod +x pkg2appimage/appdir-lint.sh
 desktop-file-validate appdir/*.desktop || exit $?
-./pkg2appimage/appdir-lint.sh ./appdir || exit $?
+
+VALIDATE_ARG=""
+if appstreamcli --help > /dev/null; then
+    if test "`appstreamcli --version`" != "AppStream CLI tool version: 0.9.4"; then
+        ./pkg2appimage/appdir-lint.sh ./appdir || exit $?
+    else
+        # old version does not support content_rating, transform to something compliant for validation
+        # this case can be removed once we bump the base build system to Ubuntu 20.04 or 18.04
+        cp -a appdir appdir_validate || exit $?
+        find appdir_validate -name *.appdata.xml -exec sed -i \{\} -e s/content_rating/x-content_rating/ \;
+        ./pkg2appimage/appdir-lint.sh ./appdir_validate || exit $?
+        rm -rf appdir_validate
+        VALIDATE_ARG="-n"
+    fi
+fi
 
 #   pack it up. The appimagetool package is available here:
 #   https://github.com/AppImage/AppImageKit/releases
 
 rm -f $1
 rm -f appdir/*-handler.desktop
-ARCH=${ARCH} appimagetool-${ARCH}.AppImage --appimage-extract-and-run appdir $1 || exit $?
+ARCH=${ARCH} appimagetool-${ARCH}.AppImage --appimage-extract-and-run ${VALIDATE_ARG} appdir $1 || exit $?
 
 # test that the package runs with and without system libraries
 ./$1 --appimage-extract --version || exit $?
