6ae2980e9081d37232dd988a0628384150428b33
Manuel Moos <z-man@users.sf.net>
2021-01-05 06:26:44 +0100

    Distinguish staged/unstaged docker deployment

--- docker/deploy/deploy_docker.sh
+++ docker/deploy/deploy_docker.sh
@@ -7,8 +7,15 @@
 . ./context/version.sh || exit $?
 . ../deploy/targets.sh || exit $?
 
+
+if ${STAGING} == true; then
+    NAME=${PACKAGE_NAME}-staging
+else
+    NAME=${PACKAGE_NAME}
+fi
+
 REGISTRY=${CI_REGISTRY_IMAGE:-registry.gitlab.com/armagetronad/armagetronad}
-DI_TAG=${PACKAGE_NAME}:${PACKAGE_VERSION}
+DI_TAG=${NAME}:${PACKAGE_VERSION}
 
 function tag()
 {
@@ -21,5 +28,5 @@ docker build -t ${DI_TAG} "$1" || exit $?
 if test "${ZI_SERIES}" = "stable" || test "${ZI_SERIES}" = "rc"; then
     tag ${REGISTRY}/${DI_TAG} || exit $?
 fi
-tag ${REGISTRY}/${PACKAGE_NAME} || exit $?
+tag ${REGISTRY}/${NAME} || exit $?
 docker image rm ${DI_TAG} || exit $?
