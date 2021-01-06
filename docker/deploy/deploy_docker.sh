80c338b56acaba0f98d16d6fb0d0f425d30a8c96
Manuel Moos <z-man@users.sf.net>
2021-01-06 23:15:17 +0100

    Docker: Use remote cache

--- docker/deploy/deploy_docker.sh
+++ docker/deploy/deploy_docker.sh
@@ -9,24 +9,57 @@
 
 
 if ${STAGING} == true; then
-    NAME=${PACKAGE_NAME}-staging
+	NAME=${PACKAGE_NAME}-staging
 else
-    NAME=${PACKAGE_NAME}
+	NAME=${PACKAGE_NAME}
 fi
 
 REGISTRY=${CI_REGISTRY_IMAGE:-registry.gitlab.com/armagetronad/armagetronad}
 DI_TAG=${NAME}:${PACKAGE_VERSION}
+CACHE=${REGISTRY}/cache:${PACKAGE_NAME}
+BASE_CACHE=${REGISTRY}/cache:armagetronad
 
 function tag()
 {
-    docker tag ${DI_TAG} $1 || exit $?
-    docker push $1 || exit $?
+	docker tag ${DI_TAG} $1 || exit $?
+	if test x${CI_COMMIT_REF_PROTECTED} = xtrue; then
+		docker push $1 || exit $?
+	fi
 }
 
-set -x
-docker build -t ${DI_TAG} "$1" || exit $?
-if test "${ZI_SERIES}" = "stable" || test "${ZI_SERIES}" = "rc"; then
-    tag ${REGISTRY}/${DI_TAG} || exit $?
+BUILDARGS="$1"
+PAST_CACHE_ARGS=""
+function build()
+{
+	TARGET=$1
+
+	CACHE_ARGS="--cache-from ${CACHE}-${TARGET} \
+		--cache-from ${BASE_CACHE}-${TARGET}"
+
+	set -x
+	docker pull ${CACHE}-${TARGET} || \
+	docker pull ${BASE_CACHE}-${TARGET} || \
+	CACHE_ARGS=""
+
+	docker build --target ${TARGET}\
+		-t ${DI_TAG} ${CACHE_ARGS} ${PAST_CACHE_ARGS} ${BUILDARGS} || exit $?
+	tag ${CACHE}-${TARGET} || exit $?
+	PAST_CACHE_ARGS="${PAST_CACHE_ARGS} --cache-from ${CACHE}-${TARGET}"
+}
+
+build build
+build runtime
+#build run_server
+
+# final build
+docker build \
+	-t ${DI_TAG} ${PAST_CACHE_ARGS} ${BUILDARGS} || exit $?
+
+if test -z "${DOCKER_NODEPLOY}"; then
+	if test "${ZI_SERIES}" = "stable" || test "${ZI_SERIES}" = "rc"; then
+		tag ${REGISTRY}/${DI_TAG} || exit $?
+	fi
+	tag ${REGISTRY}/${NAME} || exit $?
 fi
-tag ${REGISTRY}/${NAME} || exit $?
+
 docker image rm ${DI_TAG} || exit $?
