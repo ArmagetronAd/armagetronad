ccbef7a3342f3815e4ae93e2d4100b6cb9a05a32
Manuel Moos <z-man@users.sf.net>
2021-02-14 22:24:15 +0100

    Update macOS build reference

--- docker/scripts/fix_gits.sh
+++ docker/scripts/fix_gits.sh
@@ -18,7 +18,7 @@ function fix_gits(){
 
     set -x
 
-    fix_git macOS c77e2d9a24e48f36f1ddab7c76f140e7f145ccf1 || return $?
+    fix_git macOS 279af5a4086248e428682c2f2bd0f08c4d049e45 || return $?
     fix_git winlibs d03e20bf8973a6cbd1e0ecb47dd8333c5614b139 || return $?
     fix_git codeblocks 5ab4c626eb7cfa692a0bda7dbf26fed3721e4aa3 || return $?
     fix_git ubuntu c4910e573fefd62f8241d4a484482ab8bb205b4d || return $?
