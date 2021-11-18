eb83f459bfa60ee2f2a9c0464bbc5a0881549d09
Manuel Moos <z-man@users.sf.net>
2021-11-18 19:21:41 +0100

    Make sound buffer length depend on samping rate
    Decrease default buffer length to 1, resulting in 120 calls to
    the mixer function per second independently of the audio sampling
    rate. This reduces sound delays.

--- src/engine/eSoundMixer.cpp
+++ src/engine/eSoundMixer.cpp
@@ -108,7 +108,7 @@ static tConfItem<int> scc("SOUND_CHANNELS",numSoundcardChannels);
 int musicActive = 1;
 static tConfItem<int> se("MUSIC_ACTIVE", musicActive);
 
-float buffersize = 4.0;
+float buffersize = 1.0;
 static tConfItem<float> sbs("SOUND_BUFFER_SIZE", buffersize);
 
 tString titleTrack("music/titletrack.ogg");
@@ -270,7 +270,7 @@ void eSoundMixer::Init() {
     int rc;
 
     // guesstimate the desired number of samples to calculate in advance
-    int samples = static_cast< int >( buffersize * 512 );
+    int samples = static_cast< int >( (buffersize * frequency) / 120 );
 
     rc = Mix_OpenAudio( frequency, AUDIO_S16LSB,
                         numSoundcardChannels, samples );
