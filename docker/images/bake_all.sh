2557c67830da106bc4c647663ec2698bbc1a6c00
Manuel Moos <z-man@users.sf.net>
2023-10-02 21:57:47 +0200

    Avoid listing images twice

--- docker/images/bake_all.sh
+++ docker/images/bake_all.sh
@@ -6,22 +6,15 @@ wd="`dirname $0`"
 . ${wd}/prefer_podman.sh || exit $?
 . ${wd}/epoch.sh || exit $?
 
-#set -x
+set -x
 
 function bake(){
     # bake digest into .gitlab-ci.yml
     sed -i ${wd}/../../.gitlab-ci.yml -e "s,image: \$CI_REGISTRY_IMAGE/$1.*,image: \$CI_REGISTRY_IMAGE/$1@`cat ${wd}/$1.digest`,"
 }
 
-bake armaroot_64
-bake armawineblocks
-bake armasteam_64
-bake armabuild_64
-bake armabuild_32
-bake armadeb_64
-bake armadeploy_64
-bake armalpine_32
-bake steamcmd
-wait
+for f in `ls ${wd}/*.digest`; do
+    bake `echo $f | sed -e s,.*/,,g -e s,/\.digest,,`
+done
 
 
