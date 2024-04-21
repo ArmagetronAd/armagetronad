e3afeea504a3becc9a990ff98f40ce0265ab53f9
Manuel Moos <z-man@users.sf.net>
2024-04-21 16:27:08 +0200

    Add old protobuf version (if installed by homebrew) to paths
    Background: protobuf 22 now requires abseil, which is absurdly big
    and we do not want it in our bundles. And even if we would be OK
    with that, bundle ceeation with later protobuf version fails and
    the bundling script would need unknown adaptions.

--- desktop/os-x/configure_for_bundle.sh
+++ desktop/os-x/configure_for_bundle.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# pick the latest protobuf 21.X version
+# we do this because 22 introduced the unwanted dependency on abseil
+OLD_PROTOBUF="/usr/local/opt/protobuf@21/"
+echo OLD_PROTOBUF=${OLD_PROTOBUF}
+if ! test -z "${OLD_PROTOBUF}"; then
+    export PKG_CONFIG_PATH=${OLD_PROTOBUF}/lib/pkgconfig:${PKG_CONFIG_PATH}
+    export PATH=${OLD_PROTOBUF}/bin:${PATH}
+    #echo PGK_CONFIG_PATH=${PKG_CONFIG_PATH}
+    #echo PATH=${PATH}
+fi
+
 $(dirname $0)/../../configure --disable-restoreold --enable-automakedefaults \
     --disable-useradd --disable-sysinstall --disable-initscripts \
     --disable-uninstall --disable-etc --disable-games \
@@ -8,3 +19,4 @@ $(dirname $0)/../../configure --disable-restoreold --enable-automakedefaults \
     --datadir=/Contents/Resources \
     --libdir=/Contents/Frameworks \
     "$@"
+
