7a5c94cd7a430a66fbd88a3795a822fe49f15bd4
Manuel Moos <z-man@users.sf.net>
2025-03-04 19:32:12 +0100

    Bump deploy base image to Ubuntu 22.04
    0install stopped working properly on 16.04, can't blame them

--- docker/images/build_armadeploy_64.sh
+++ docker/images/build_armadeploy_64.sh
@@ -3,6 +3,6 @@
 # builds an deploy capable machine
 
 wd="`dirname $0`"
-${wd}/build_armabuild.sh amd64/ubuntu:16.04 armadeploy_64 --target armadeploy
+${wd}/build_armabuild.sh amd64/ubuntu:22.04 armadeploy_64 --target armadeploy
 
 
