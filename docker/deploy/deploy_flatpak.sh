4960e32b899646f0c39400e0cbb8d72e26666a3f
Manuel Moos <z-man@users.sf.net>
2025-03-07 23:53:10 +0100

    Don't mess with the shared_modules submodule
    The missing update reverted it to some old state

--- docker/deploy/deploy_flatpak.sh
+++ docker/deploy/deploy_flatpak.sh
@@ -25,6 +25,7 @@ pushd flatpak || exit $?
 # go back to last human edit
 git checkout ${BRANCH_BASE}_${VERSION_SERIES}_ci || exit $?
 git reset origin/${BRANCH_BASE}_${VERSION_SERIES} --hard || exit $?
+git submodule update --checkout || exit $?
 
 FILENAME=${PACKAGE_NAME}-${PACKAGE_VERSION}.tbz
 
@@ -45,6 +46,8 @@ fi
 git commit . -m "Update to version ${PACKAGE_VERSION}" || exit $?
 if ! test ${STAGING} == true; then
     git push --force || exit $?
+else
+	git log -p -2
 fi
 popd
 
