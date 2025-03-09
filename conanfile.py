95e14d49bdfbd73485a786e465fddcd0b2a1525e
Manuel Moos <z-man@users.sf.net>
2025-03-09 12:21:30 +0100

    Humble beginnings: conanfile for curl.

---
+++ conanfile.py
@@ -0,0 +1,25 @@
+from conan import ConanFile
+
+# activate with
+# conan install . --build=missing 
+
+class Pkg(ConanFile):
+    generators = "AutotoolsToolchain", "PkgConfigDeps"
+
+    requires = "libcurl/[>=7]"
+
+    default_options = {
+        "libcurl/*:with_ssl": False,
+        "libcurl/*:with_https": False,
+        "libcurl/*:with_ftp": False,
+        "libcurl/*:with_file": False,
+        "libcurl/*:with_rtsp": False,
+        "libcurl/*:with_dict": False,
+        "libcurl/*:with_telnet": False,
+        "libcurl/*:with_tftp": False,
+        "libcurl/*:with_pop3": False,
+        "libcurl/*:with_imap": False,
+        "libcurl/*:with_smtp": False,
+        "libcurl/*:with_gopher": False
+    }
+
