5fed8950c0f2dc8fff2abc16f67c76a7698c8515
Manuel Moos <z-man@users.sf.net>
2026-03-07 00:46:23 +0100

    Move cmath include to the right spot
    It is used in tCoord.h already, clang is correct to complain

--- src/tools/tCoord.h
+++ src/tools/tCoord.h
@@ -35,6 +35,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <algorithm>
 #include <map>
 #include <vector>
+#include <cmath>
 
 namespace Tools { class Direction; class Position; }
 
