38e9613a01c5dc1b23a4883c2bc04dc94f5c729b
Manuel Moos <z-man@users.sf.net>
2026-03-13 21:56:57 +0100

    Launchpad upload should now work with modern Ubuntu

--- docker/images/upload_all.sh
+++ docker/images/upload_all.sh
@@ -36,9 +36,9 @@ upload_image armaroot_64 $@ || exit $?
 upload_image armawineblocks $@ || exit $?
 upload_image armasteam_64 $@ || exit $?
 upload_image armadeb_64 $@ || exit $?
-upload_image armadeploy_16 $@ || exit $?
+# upload_image armadeploy_16 $@ || exit $? # unused
 upload_image armadeploy_20 $@ || exit $?
-# upload_image armadeploy_24 $@ || exit $? # unused
+upload_image armadeploy_24 $@ || exit $?
 upload_image steamcmd $@ || exit $?
 
 upload_image armabuild_32 $@ || exit $?
