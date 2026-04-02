ca0b8e3e5aed7b20bb619867192f2ce6bf2fce54
Manuel Moos <z-man@users.sf.net>
2026-04-02 22:58:42 +0200

    Include dependency bin dirs to path

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 from conan.tools.files import save, copy
-from conan.tools.gnu import PkgConfigDeps
-from conan.tools.env import VirtualRunEnv, VirtualBuildEnv
+from conan.tools.gnu import PkgConfigDeps, AutotoolsToolchain
+from conan.tools.env import VirtualRunEnv, VirtualBuildEnv, Environment
 import os
 
 ##################
@@ -12,9 +12,6 @@ import os
 # ./conan_install.sh
 
 class Pkg(ConanFile):
-    generators = \
-            "AutotoolsToolchain"
-
 
     requires = \
             "sdl_aa/[>=1.2.15 <2.0.0]", \
@@ -49,9 +46,6 @@ class Pkg(ConanFile):
     keep_imports = True
 
     def generate(self):
-        venv = VirtualBuildEnv(self)
-        venv.generate()
-
         pc = PkgConfigDeps(self)
         pc.generate()
 
@@ -69,17 +63,67 @@ Cflags:
         #none_pc = os.path.join(none_pkg.package_folder, "lib", "pkgconfig", "none.pc")
         #copy(self, "none.pc", os.path.dirname(none_pc), self.build_folder)
 
+        tc = AutotoolsToolchain(self)
+
         # copy libraries
         libs_path = os.path.join(self.build_folder, "lib")
+        os.makedirs(libs_path, exist_ok=True)
+        print(libs_path)
         for dep_name, dep in self.dependencies.items():
             dirs = dep.cpp_info.libdirs + dep.cpp_info.bindirs
+            print(dirs)
             for dir in dirs:
-                for extension in [ "*.so.*", "*.dylib*", "*.dll" ]:
+                for extension in [ "*.so", "*.so.*", "*.dylib", "*.dylib*", "*.dll" ]:
                     copy(self, extension, dir, libs_path)
+
+        build_env = VirtualBuildEnv(self)
+        build_env.environment().prepend_path("LD_LIBRARY_PATH", libs_path)
+        build_env.environment().prepend_path("DYLD_LIBRARY_PATH", libs_path)
+        build_env.environment().prepend_path("DYLD_FALLBACK_LIBRARY_PATH", libs_path)
 
         # modiy LD_LIBRARY_PATH
         run_env = VirtualRunEnv(self)
-        run_env.environment().append_path("LD_LIBRARY_PATH", libs_path)
-        run_env.environment().append_path("LC_RPATH_PATH", libs_path)
+        run_env.environment().prepend_path("LD_LIBRARY_PATH", libs_path)
+        run_env.environment().prepend_path("DYLD_LIBRARY_PATH", libs_path)
+        run_env.environment().prepend_path("DYLD_FALLBACK_LIBRARY_PATH", libs_path)
+
+        # hack in dependency library paths
+        bin_paths = [lib for _, dep in self.dependencies.items() for lib in dep.cpp_info.bindirs]
+        lib_paths = [lib for _, dep in self.dependencies.items() for lib in dep.cpp_info.libdirs]
+        include_paths = [lib for _, dep in self.dependencies.items() for lib in dep.cpp_info.includedirs]
+        include_statements = [" -I" + inc for inc in include_paths]
+        lib_statements = [" -L" + lib for lib in lib_paths]
+        pkg_config_paths = [os.path.join(lib, "pkgconfig") for lib in lib_paths]
+
+        env = Environment()
+        env.define("CPPFLAGS", os.environ.get("CPPFLAGS", "") + " " + " ".join(include_statements))
+
+        rpath_statements = ["-Wl,-rpath," + lib for lib in lib_paths]
+        env.define("LDFLAGS", os.environ.get("LDFLAGS", "") + " " + " ".join(lib_statements + rpath_statements))
+
+        env.append_path("LIBRARY_PATH", os.pathsep.join(lib_paths))
+        env.append_path("LD_LIBRARY_PATH", os.pathsep.join(lib_paths))
+        env.append_path("PKG_CONFIG_PATH", os.pathsep.join(pkg_config_paths))
+        env = env.vars(self, scope="build")
+
+        build_env.environment().define("CPPFLAGS", os.environ.get("CPPFLAGS", "") + " " + " ".join(include_statements))
+        build_env.environment().define("LDFLAGS", os.environ.get("LDFLAGS", "") + " " + " ".join(lib_statements + rpath_statements))
+        build_env.environment().append_path("PATH", os.pathsep.join(bin_paths))
+
+        # set SDL prefix explicitly to avoid finding system SDL
+        sdl = self.dependencies["sdl_aa"]
+        #print(sdl.cpp_info.includedirs)
+        #exit(1)
+
+        #png = self.dependencies["libpng"]
+        tc.configure_args.extend([
+            "--with-sdl-prefix=" + sdl.package_folder,
+            "--disable-imageio",
+        ])
+
+        env.save_script("aa_env")
+
+        tc.generate()
         run_env.generate()
+        build_env.generate()
 
