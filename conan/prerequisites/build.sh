81a71805306686e8dbee917771bcf70027427010
Manuel Moos <z-man@users.sf.net>
2026-07-11 00:25:58 +0200

    Improve build script, write documentation

--- conan/prerequisites/build.sh
+++ conan/prerequisites/build.sh
@@ -4,16 +4,9 @@
 
 cd `dirname $0`
 
-yes | conan remove sdl_aa
-yes | conan remove aa_libsdl
-yes | conan remove libsdl
-yes | conan remove sdl
+conan install sdl --build=missing
 conan create sdl --build=missing
 
-yes | conan remove sdl_image_aa
-yes | conan remove aa_libsdl_image
-yes | conan remove libsdl_image
-yes | conan remove sdl_image
 conan install sdl_image --build=missing
 conan create sdl_image --build=missing
 
